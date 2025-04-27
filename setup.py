91b75bf12bd366281018af6f3d1b2d197b6fe819
[Ali Hassan] <[ah1087838@gmail.coml]>
2025-04-27 09:47:49 +0500

    folder structure added

--- setup.py
+++ setup.py
@@ -3,8 +3,24 @@ from setuptools import find_packages,setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Ali Hassan',
+    author= 'Hassan',
     author_email= 'ah1087838@gmail.com',
     packages= find_packages(),
     install_requires = []
 )
+
+
+
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="medical-end-to-end-chatbot-project",
+#     version="0.0.1",
+#     author="Hassan",
+#     author_email="your_email@example.com",
+#     description="An end-to-end medical chatbot project using LangChain, Pinecone, and OpenAI.",
+#     packages=find_packages(),
+#     install_requires=[],
+# )
